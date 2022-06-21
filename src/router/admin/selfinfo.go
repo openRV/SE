1524611539fca9e08a524d6d22d07d1810318ce2
逾白 <8732742+kinrin@user.noreply.gitee.com>
2022-06-21 18:54:55 +0800

    Add selfinfo

---
+++ src/router/admin/selfinfo.go
@@ -0,0 +1,35 @@
+package admin
+
+import (
+	"SE/src/Interface"
+	"SE/src/Interface/admin/index"
+	"SE/src/database"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func SelfInfo(c *gin.Context) {
+	userName := c.GetHeader("Uaername")
+
+	if !database.SearchUser(database.User{Username: userName, Password: ""}).Exist {
+		c.IndentedJSON(http.StatusOK,
+			Interface.ErrorRes{Success: false, Msg: "user does not exist"})
+		return
+	}
+
+	ret := database.GetSelfInfo(userName)
+	res := index.SelfInfoResult{
+		Success: true,
+		Data: struct {
+			UserName string `json:"userName"`
+			Password string `json:"password"`
+			Avatar   string `json:"avatar"`
+		}(struct {
+			UserName string
+			Password string
+			Avatar   string
+		}{UserName: ret.UserName, Password: ret.Password, Avatar: ret.Avatar}),
+	}
+
+	c.IndentedJSON(http.StatusOK, res)
+}
