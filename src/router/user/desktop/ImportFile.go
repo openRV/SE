181eb63ad6180a25f8ce7781db9994502e3dbfc4
dupeiran <2083856903@qq.com>
2022-06-22 14:17:52 +0800

    tested upload file

--- src/router/user/desktop/ImportFile.go
+++ src/router/user/desktop/ImportFile.go
@@ -13,9 +13,34 @@ func ImportFile(c *gin.Context) {
 	file, err := c.FormFile("FormData")
 	if err != nil {
 		c.IndentedJSON(http.StatusOK, comInterface.ErrorRes{Success: false, Msg: "upload error"})
+		return
 	}
 
 	fileName := file.Filename
 	fmt.Println(fileName)
+
+	f, err := file.Open()
+	if err != nil {
+		c.IndentedJSON(http.StatusOK, comInterface.ErrorRes{Success: false, Msg: "upload error"})
+		return
+	}
+	defer f.Close()
+	buf := make([]byte, file.Size)
+	var chunk []byte
+	for {
+		n, err := f.Read(buf)
+		if err != nil && err.Error() != "EOF" {
+			fmt.Println(err)
+			c.IndentedJSON(http.StatusOK, comInterface.ErrorRes{Success: false, Msg: "upload error"})
+			return
+		}
+		if n == 0 {
+			break
+		}
+		chunk = append(chunk, buf[:n]...)
+	}
+
+	fmt.Println(string(chunk))
+
 	c.IndentedJSON(http.StatusOK, comInterface.ErrorRes{Success: true, Msg: fileName})
 }
